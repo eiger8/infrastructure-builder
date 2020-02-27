9da114eae80e2ebd6518a3065890d0b791e3e642
eiger8 <oleksa.oryshchyn@gmail.com>
2020-02-27 17:31:48 +0200

    FIX & Tuneup connected with count and multiple items creation

--- main.tf
+++ main.tf
@@ -2,23 +2,20 @@ module "env-network" {
   source = "./modules/network"
   env-name = var.env-name
   ip-allowed = var.ip-allowed
-//  private-subnet = var.private-subnet
-//  public-subnet = var.public-subnet
   region = var.region
   vpc-cidr = var.vpc-cidr
   zones = var.zones
-  nat-instance-id = aws_instance.nat-bastion[*].id
+  nat-instance-id = aws_instance.nat-bastion.id
 }
 provider "aws" {
   region = var.region
 }
 //T2 Micro instance. NAT Instance & Bastion Host.
 resource "aws_instance" "nat-bastion" {
-  count = length(var.zones)
   ami = "ami-009816cdbb1e74ceb"
   instance_type = "t2.micro"
   key_name = var.ssh_key["name"]
-  subnet_id = element(module.env-network.env-private-subnet-id[*], count.index)
+  subnet_id = module.env-network.env-public-subnet-id[0]
   vpc_security_group_ids = [module.env-network.env-security-group-pub-id]
   source_dest_check = "false"
   tags = {
@@ -31,15 +28,14 @@ resource "aws_instance" "nat-bastion" {
     host     = self.public_dns
   }
 }
-resource "aws_instance" "instance-vulgaris" {
-  count = length(var.zones)
+resource "aws_instance" "regular-amz-t2micro" {
   ami = "ami-0a85857bfc5345c38"
   instance_type = "t2.micro"
   key_name = var.ssh_key["name"]
-  subnet_id = element(module.env-network.env-private-subnet-id[*], count.index)
+  subnet_id = module.env-network.env-private-subnet-id[0]
   vpc_security_group_ids = [module.env-network.env-security-group-priv-id]
   tags = {
-    Name = "AMZ Vulgaris ${var.env-name}"
+    Name = "AMZ T2 Micro ${var.env-name}"
   }
   connection {
     type     = "ssh"
